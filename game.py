8943285c8f8ce7f43986bda8c4a9c1f3a71f034f
Ayush <ayushnautiyal1110@gmail.com>
2023-06-22 20:17:43 +0530

    Snake Water Gun

--- game.py
+++ game.py
@@ -3,39 +3,39 @@ st.write("=================== Welcome to Snake Water Gun Game ================="
 import random
 import math
 no=random.randint(1,3)
-st.write("Hey Please enter the number")
-inp=int(input())
+
+inp=st.number_input("Hey Please enter the number")
 You=0
 Computer=0
 while(True):
     if(inp==0):
-        st.write("Thanks For Playing the Game")
-        st.write("Here is score")
-        st.write(You)
-        st.write("Here is Computer score")
-        st.write(Computer)
+        st.markdown("Thanks For Playing the Game")
+        st.markdown("Here is score")
+        st.markdown(You)
+        st.markdown("Here is Computer score")
+        st.markdown(Computer)
         break;
     elif(no==inp):
-        st.write("Draw")
+        st.markdown("Draw")
     elif(no==1):
         if(inp==2):
-            st.write("You Lost")
+            st.markdown("You Lost")
             Computer+=1
         else:
-            st.write("You Won")
+            st.markdown("You Won")
             You+=1
     elif(no==2):
         if(inp==1):
-            st.write("You Won")
+            st.markdown("You Won")
             You+=1
         else:
-            st.write("You Lost")
+            st.markdown("You Lost")
             Computer+=1
     elif(no==3):
         if(inp==1):
-            st.write("You Lost")
+            st.markdown("You Lost")
             Computer+=1
         else:
-            st.write("You Won")
+            st.markdown("You Won")
             You+=1
             
